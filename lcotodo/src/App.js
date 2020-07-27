2f21ed5d5edd4f4e42da8cc46f5ce04041e78904
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-07-27 14:37:04 +0100

    practice

--- lcotodo/src/App.js
+++ lcotodo/src/App.js
@@ -6,20 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className = "App-logo"/>
+        <p>Learnign React with Omario</p>
       </header>
+      
     </div>
+    
   );
 }
 
